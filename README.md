f88d7aef36c2c51beed1fb7e9897a62e88af3113
agelostsal <agelos.tsal@gmail.com>
2018-11-16 12:15:29 +0200

    ARGO-1493 Initialise ams-push-server ARGO-1494 Implement activate subscription functionality for ams-push-server

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# ams-push-server
-Argo Messaging Service Push Server
+# Ams Push Server
+The ams push server is a component that handles the push functionality for the respective 
+push enabled subscriptions in the [argo-messaging-service.](https://github.com/ARGOeu/argo-messaging-service)
+
+## Managing the protocol buffers and gRPC definitions
+
+In order to modify the any `.proto` file you will need the following
+
+ - Read on how to install the protoc compiler on your platform [here.](https://github.com/protocolbuffers/protobuf)
+ 
+ -  Install the go plugin. `go get -u github.com/golang/protobuf/protoc-gen-go`
+ 
+ - install the go gRPC package. `go get -u google.golang.org/grpc`
+ 
+ - Inside `api/<version>/grpc` compile. `protoc -I proto/ proto/ams.proto --go_out=plugins=grpc:proto`
+
+## Building and running the ams push server
+In the root of the directory issue:
+```commandline
+go build && ./ams-push-server
+```
+
+This will build and run the server binding on port `:5555`.
---
+++ main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	amsgRPC "github.com/ARGOeu/ams-push-server/api/v1/grpc"
+	"github.com/grpc-ecosystem/go-grpc-middleware"
+	"github.com/grpc-ecosystem/go-grpc-middleware/logging/logrus"
+	"github.com/grpc-ecosystem/go-grpc-middleware/tags"
+	"github.com/sirupsen/logrus"
+	lSyslog "github.com/sirupsen/logrus/hooks/syslog"
+	"log/syslog"
+	"net"
+	"time"
+)
+
+var LOGGER = logrus.New()
+
+func init() {
+	LOGGER.SetFormatter(&logrus.TextFormatter{FullTimestamp: true, DisableColors: true})
+	hook, err := lSyslog.NewSyslogHook("", "", syslog.LOG_INFO, "")
+	if err == nil {
+		LOGGER.AddHook(hook)
+	}
+}
+
+func main() {
+
+	opts := []grpc_logrus.Option{
+		grpc_logrus.WithDurationField(func(duration time.Duration) (key string, value interface{}) {
+			return "grpc.time_ns", duration.Nanoseconds()
+		}),
+	}
+
+	srvOpts := grpc_middleware.WithUnaryServerChain(
+		grpc_ctxtags.UnaryServerInterceptor(),
+		grpc_logrus.UnaryServerInterceptor(logrus.NewEntry(LOGGER), opts...),
+	)
+
+	listener, err := net.Listen("tcp", ":5555")
+	if err != nil {
+		LOGGER.Fatalf("Could not listen, %v", err.Error())
+	}
+
+	srv := amsgRPC.NewGRPCServer(srvOpts)
+
+	LOGGER.Infoln("API    gRPC Server will serve on port: 5555")
+
+	defer func() {
+		listener.Close()
+		srv.GracefulStop()
+	}()
+
+	err = srv.Serve(listener)
+
+	if err != nil {
+		LOGGER.Fatalf("Could not serve, %v", err.Error())
+	}
+}
