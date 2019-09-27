2be843856ee487d02aa168a43552dd6c28060b6b
agelostsal <agelos.tsal@gmail.com>
2019-09-27 22:28:28 +0300

    ARGO-1948 Add new push worker implementation for slow start retry policy

--- push/worker.go
+++ push/worker.go
@@ -1,10 +1,14 @@
 package push
 
 import (
+	"context"
 	"fmt"
 	amsPb "github.com/ARGOeu/ams-push-server/api/v1/grpc/proto"
 	"github.com/ARGOeu/ams-push-server/consumers"
+	"github.com/ARGOeu/ams-push-server/retrypolicies"
 	"github.com/ARGOeu/ams-push-server/senders"
+	log "github.com/sirupsen/logrus"
+	"time"
 )
 
 type PushError struct {
@@ -31,10 +35,178 @@ type Worker interface {
 // New acts as a worker factory, creates and returns a new worker based on the provided type
 func New(sub *amsPb.Subscription, c consumers.Consumer, s senders.Sender, ch chan<- consumers.CancelableError) (Worker, error) {
 
-	switch sub.PushConfig.RetryPolicy.Type {
-	case "linear":
-		return NewLinearWorker(sub, c, s, ch), nil
+	rp, err := retrypolicies.New(sub.PushConfig.RetryPolicy)
+	if err != nil {
+		return nil, fmt.Errorf("worker %v not yet implemented", sub.PushConfig.RetryPolicy.Type)
+
 	}
 
-	return nil, fmt.Errorf("worker %v not yet implemented", sub.PushConfig.RetryPolicy.Type)
+	w := new(worker)
+
+	parentCtx := context.TODO()
+	ctx, cancel := context.WithCancel(parentCtx)
+
+	w.sub = sub
+	w.consumer = c
+	w.sender = s
+	w.retryPolicy = rp
+	w.ctx = ctx
+	w.cancel = cancel
+	w.deactivationChan = ch
+
+	return w, nil
+
+}
+
+// worker implements the Worker interface
+type worker struct {
+	sub              *amsPb.Subscription
+	consumer         consumers.Consumer
+	sender           senders.Sender
+	cancel           context.CancelFunc
+	ctx              context.Context
+	retryPolicy      retrypolicies.RetryPolicy
+	deactivationChan chan<- consumers.CancelableError
+	pushErr          string
+}
+
+// Consumer returns the currently in use consumer
+func (w *worker) Consumer() consumers.Consumer {
+	return w.consumer
+}
+
+// Status returns whether or not the worker is experiencing any error handling its assigned subscription
+func (w *worker) Status() string {
+	if w.pushErr == "" {
+		return fmt.Sprintf("Subscription %v is currently active", w.sub.FullName)
+	}
+
+	return w.pushErr
+}
+
+// Subscription returns the currently active subscription inside the worker
+func (w *worker) Subscription() *amsPb.Subscription {
+	return w.sub
+}
+
+// Start starts the push functionality for the worker
+func (w *worker) Start() {
+
+Loop:
+	for {
+		select {
+		case <-w.retryPolicy.Timer().C:
+			w.push()
+		case <-w.ctx.Done():
+			canceled := w.retryPolicy.Timer().Stop()
+
+			if !canceled {
+				<-w.retryPolicy.Timer().C
+			}
+
+			break Loop
+		}
+
+		w.retryPolicy.Reset(w.pushErr)
+	}
+}
+
+// push executes the push cycle of consume -> send -> ack
+func (w *worker) push() {
+
+	rml, err := w.consumer.Consume(w.ctx, w.sub.PushConfig.MaxMessages)
+	if err != nil {
+
+		ce, ok := w.consumer.ToCancelableError(err)
+		if ok {
+			w.deactivationChan <- ce
+			return
+		}
+
+		if err.Error() == "no new messages" {
+			log.WithFields(
+				log.Fields{
+					"type":     "service_log",
+					"resource": w.consumer.ResourceInfo(),
+				},
+			).Debug("No new messages")
+			return
+		}
+
+		log.WithFields(
+			log.Fields{
+				"type":     "service_log",
+				"resource": w.consumer.ResourceInfo(),
+				"error":    err.Error(),
+			},
+		).Error("Could not consume message")
+
+		w.pushErr = fmt.Sprintf(
+			"%v - %v, %v",
+			time.Now().UTC().Format("2006-01-02T15:04:05"),
+			"Could not consume message",
+			err.Error(),
+		)
+
+		return
+	}
+
+	pms := senders.PushMsgs{}
+
+	for _, rm := range rml.RecMsgs {
+		msg := senders.PushMsg{
+			Sub: w.consumer.ResourceInfo(),
+			Msg: rm.Msg,
+		}
+
+		pms.Messages = append(pms.Messages, msg)
+	}
+
+	err = w.sender.Send(w.ctx, pms, senders.DetermineMessageFormat(w.sub.PushConfig.MaxMessages))
+	if err != nil {
+		log.WithFields(
+			log.Fields{
+				"type":     "service_log",
+				"endpoint": w.sender.Destination(),
+				"error":    err.Error(),
+			},
+		).Error("Could not send message")
+
+		w.pushErr = fmt.Sprintf(
+			"%v - %v, %v",
+			time.Now().UTC().Format("2006-01-02T15:04:05"),
+			"Could not send message",
+			err.Error(),
+		)
+
+		return
+	}
+
+	err = w.consumer.Ack(w.ctx, rml.Last().AckID)
+	if err != nil {
+
+		log.WithFields(
+			log.Fields{
+				"type":  "service_log",
+				"error": err.Error(),
+			},
+		).Error("Could not acknowledge message")
+
+		w.pushErr = fmt.Sprintf(
+			"%v - %v, %v",
+			time.Now().UTC().Format("2006-01-02T15:04:05"),
+			"Could not acknowledge message",
+			err.Error(),
+		)
+
+		return
+	}
+
+	// if no errors occurred during the push cycle make sure that there is no error registered
+	w.pushErr = ""
+}
+
+// Stop stops the push worker's functionality
+func (w *worker) Stop() {
+	w.cancel()
 }
--- push/worker_test.go
+++ push/worker_test.go
@@ -1,11 +1,16 @@
 package push
 
 import (
+	"context"
+	"fmt"
 	amsPb "github.com/ARGOeu/ams-push-server/api/v1/grpc/proto"
 	"github.com/ARGOeu/ams-push-server/consumers"
+	"github.com/ARGOeu/ams-push-server/retrypolicies"
 	"github.com/ARGOeu/ams-push-server/senders"
 	"github.com/stretchr/testify/suite"
+	"net/http"
 	"testing"
+	"time"
 )
 
 type WorkerTestSuite struct {
@@ -15,24 +20,288 @@ type WorkerTestSuite struct {
 // TestNew tests that the worker factory behaves properly
 func (suite *WorkerTestSuite) TestNew() {
 
-	s1 := &amsPb.Subscription{
+	c := consumers.NewAmsHttpConsumer("", "", "", &http.Client{})
+	s := senders.NewHttpSender("", &http.Client{})
+	sub := &amsPb.Subscription{
 		PushConfig: &amsPb.PushConfig{
+			MaxMessages: 1,
 			RetryPolicy: &amsPb.RetryPolicy{
-				Type: "linear",
+				Period: 300,
+				Type:   retrypolicies.LinearRetryPolicy,
 			},
 		},
 	}
 
 	// normal creation
-	w1, err1 := New(s1, &consumers.MockConsumer{}, &senders.MockSender{}, make(chan consumers.CancelableError))
-	suite.IsType(&LinearWorker{}, w1)
+
+	w, err1 := New(sub, c, s, make(chan consumers.CancelableError))
+
+	w1 := w.(*worker)
+	suite.Equal(sub, w1.sub)
+	suite.Equal(c, w1.consumer)
+	suite.Equal(s, w1.sender)
+	suite.IsType(&retrypolicies.Linear{}, w1.retryPolicy)
+	suite.NotNil(w1.cancel)
+	suite.NotNil(w1.ctx)
+	suite.IsType(&worker{}, w1)
 	suite.Nil(err1)
 
 	// unimplemented worker type
-	s1.PushConfig.RetryPolicy.Type = "unknown"
-	w2, err2 := New(s1, nil, nil, nil)
+	sub.PushConfig.RetryPolicy.Type = "unknown"
+	w2, err2 := New(sub, nil, nil, nil)
 	suite.Equal("worker unknown not yet implemented", err2.Error())
 	suite.Nil(w2)
+}
+
+// TestStartStopCycle checks the correct functionality of starting and stopping a push worker
+func (suite *WorkerTestSuite) TestStartStopCycle() {
+
+	// how often does a push cycle occurs in milliseconds
+	// a full push cycle: consume -> send -> ack
+	rate := uint32(300)
+
+	// for how long the push worker will perform in seconds
+	workerLifeTime := 1
+
+	ctx, cancel := context.WithCancel(context.TODO())
+	sub := &amsPb.Subscription{
+		PushConfig: &amsPb.PushConfig{
+			MaxMessages: 1,
+			RetryPolicy: &amsPb.RetryPolicy{
+				Period: rate,
+				Type:   retrypolicies.LinearRetryPolicy,
+			},
+		},
+	}
+
+	rp, _ := retrypolicies.New(sub.PushConfig.RetryPolicy)
+
+	c := new(consumers.MockConsumer)
+	c.SubStatus = "normal_sub"
+	c.AckStatus = "normal_ack"
+	s := new(senders.MockSender)
+
+	lw := worker{
+		sub:         sub,
+		consumer:    c,
+		sender:      s,
+		retryPolicy: rp,
+		ctx:         ctx,
+		cancel:      cancel,
+	}
+
+	// signal the push worker to stop after our predefined lifetime duration
+	time.AfterFunc(time.Duration(workerLifeTime)*time.Second, func() {
+		lw.Stop()
+	})
+
+	// start the push worker
+	lw.Start()
+
+	// since the lifetime is 1 second and the push cycle is every 300ms
+	// expect 4 elements, one at each timestamp
+	// 0ms, 300ms, 600ms, 900ms
+	suite.True(len(c.AckMessages) == 4, fmt.Sprintf("Expected 4 messages but got %v", len(c.AckMessages)))
+	suite.Equal([]string{"ackid_0", "ackid_1", "ackid_2", "ackid_3"}, c.AckMessages)
+
+	// for each pair of messages check if the time delta between them
+	// is greater or equals with the rate that we have defined
+	for idx, msg := range c.GeneratedMessages {
+
+		// last element
+		if idx == len(c.GeneratedMessages)-1 {
+			break
+		}
+
+		msgTime, _ := time.Parse(time.StampNano, msg.Msg.PubTime)
+
+		nextMsgTime, _ := time.Parse(time.StampNano, c.GeneratedMessages[idx+1].Msg.PubTime)
+
+		timeDelta := nextMsgTime.Sub(msgTime)
+
+		suite.True(timeDelta >= time.Duration(rate)*time.Millisecond)
+	}
+
+	// check that the lifetime was indeed no more than 1 second
+	// by comparing the publish time of the first and last messages
+	startTime, _ := time.Parse(time.StampNano, c.GeneratedMessages[0].Msg.PubTime)
+	endTime, _ := time.Parse(time.StampNano, c.GeneratedMessages[len(c.GeneratedMessages)-1].Msg.PubTime)
+
+	timeDelta := endTime.Sub(startTime)
+
+	suite.True(timeDelta <= time.Duration(workerLifeTime)*time.Second)
+}
+
+func (suite *WorkerTestSuite) TestPush() {
+
+	rate := uint32(300)
+
+	ctx, cancel := context.WithCancel(context.TODO())
+	sub := &amsPb.Subscription{
+		FullName: "sub1",
+		PushConfig: &amsPb.PushConfig{
+			MaxMessages: 1,
+			RetryPolicy: &amsPb.RetryPolicy{
+				Period: rate,
+				Type:   retrypolicies.LinearRetryPolicy,
+			},
+		},
+	}
+
+	rp, _ := retrypolicies.New(sub.PushConfig.RetryPolicy)
+
+	c := new(consumers.MockConsumer)
+	c.SubStatus = "normal_sub"
+	c.AckStatus = "normal_ack"
+	s := new(senders.MockSender)
+
+	lw := worker{
+		sub:         sub,
+		consumer:    c,
+		sender:      s,
+		retryPolicy: rp,
+		ctx:         ctx,
+		cancel:      cancel,
+	}
+
+	// no error - single message
+	lw.push()
+	suite.Equal(1, len(c.AckMessages))
+	suite.Equal(1, len(c.GeneratedMessages))
+	suite.Equal(1, len(s.PushMessages))
+	suite.Equal("Subscription sub1 is currently active", lw.Status())
+
+	sub2 := &amsPb.Subscription{
+		FullName: "sub1",
+		PushConfig: &amsPb.PushConfig{
+			MaxMessages: 3,
+			RetryPolicy: &amsPb.RetryPolicy{
+				Period: rate,
+				Type:   retrypolicies.LinearRetryPolicy,
+			},
+		},
+	}
+
+	rp2, _ := retrypolicies.New(sub2.PushConfig.RetryPolicy)
+
+	c2 := new(consumers.MockConsumer)
+	c2.SubStatus = "normal_sub"
+	c2.AckStatus = "normal_ack"
+	s2 := new(senders.MockSender)
+
+	lw2 := worker{
+		sub:         sub2,
+		consumer:    c2,
+		sender:      s2,
+		retryPolicy: rp2,
+		ctx:         ctx,
+		cancel:      cancel,
+	}
+
+	// no error - multiple messages
+	lw2.push()
+	suite.Equal(1, len(c2.AckMessages))
+	suite.Equal(3, len(c2.GeneratedMessages))
+	suite.Equal(3, len(s2.PushMessages))
+	suite.Equal("Subscription sub1 is currently active", lw2.Status())
+
+	// receive consumer error
+	// no message available to send
+	// no message available to ack
+	c.SubStatus = "error_sub"
+	c.GeneratedMessages = nil
+	c.AckMessages = nil
+	s.PushMessages = nil
+	lw.push()
+	suite.Equal(0, len(c.AckMessages))
+	suite.Equal(0, len(c.GeneratedMessages))
+	suite.Equal(0, len(s.PushMessages))
+	suite.Regexp("[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2} - Could not consume message, error while consuming", lw.Status())
+
+	// receive ack error
+	c.SubStatus = "normal_sub"
+	c.AckStatus = "timeout_ack"
+	c.GeneratedMessages = nil
+	c.AckMessages = nil
+	s.PushMessages = nil
+	lw.push()
+	suite.Equal(0, len(c.AckMessages))
+	suite.Equal(1, len(c.GeneratedMessages))
+	suite.Equal(1, len(s.PushMessages))
+	suite.Regexp("[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2} - Could not acknowledge message, error while acknowledging", lw.Status())
+
+	// send error
+	// no message available for ack
+	c.AckStatus = "normal_ack"
+	s.SendStatus = "error_send"
+	c.GeneratedMessages = nil
+	c.AckMessages = nil
+	s.PushMessages = nil
+	lw.push()
+	suite.Equal(0, len(c.AckMessages))
+	suite.Equal(1, len(c.GeneratedMessages))
+	suite.Equal(0, len(s.PushMessages))
+	pushErr1stCycle := lw.pushErr
+	suite.Regexp("[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2} - Could not send message, error while sending", pushErr1stCycle)
+	suite.Regexp("[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2} - Could not send message, error while sending", lw.Status())
+
+	// consume error and cancel(project not found)
+	c.SubStatus = "error_sub_no_project"
+	c.GeneratedMessages = nil
+	c.AckMessages = nil
+	s.PushMessages = nil
+	cancelCh := make(chan consumers.CancelableError, 1)
+	lw.deactivationChan = cancelCh
+	lw.push()
+	suite.Equal(0, len(c.AckMessages))
+	suite.Equal(0, len(c.GeneratedMessages))
+	suite.Equal(0, len(s.PushMessages))
+	suite.Equal(consumers.CancelableError{
+		ErrMsg:   "project doesn't exist",
+		Resource: "error_sub_no_project",
+	}, <-cancelCh)
+
+	// consume error and cancel(sub not found)
+	c.SubStatus = "error_sub_no_sub"
+	c.GeneratedMessages = nil
+	c.AckMessages = nil
+	s.PushMessages = nil
+	cancelCh2 := make(chan consumers.CancelableError, 1)
+	lw.deactivationChan = cancelCh2
+	lw.push()
+	suite.Equal(0, len(c.AckMessages))
+	suite.Equal(0, len(c.GeneratedMessages))
+	suite.Equal(0, len(s.PushMessages))
+	suite.Equal(consumers.CancelableError{
+		ErrMsg:   "Subscription doesn't exist",
+		Resource: "error_sub_no_sub",
+	}, <-cancelCh2)
+}
+
+func (suite *WorkerTestSuite) TestConsumer() {
+
+	mc := new(consumers.MockConsumer)
+	lw := worker{
+		consumer: mc,
+	}
+
+	suite.Equal(mc, lw.consumer)
+}
+
+func (suite *WorkerTestSuite) TestStatus() {
+
+	lw := worker{
+		sub: &amsPb.Subscription{
+			FullName: "sub1",
+		},
+	}
+
+	lw.pushErr = "error1"
+
+	suite.Equal("error1", lw.Status())
+
+	lw.pushErr = ""
+	suite.Equal("Subscription sub1 is currently active", lw.Status())
 }
 
 func TestWorkerTestSuite(t *testing.T) {
